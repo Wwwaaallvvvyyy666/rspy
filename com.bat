9f7827cbd79dd9e04ec4d56f9ed5fe229ab863ea
Wwwaaallvvvyyy666 <tiktokwalvy@gmail.com>
2026-08-01 13:17:56 +0700

    666's spy

--- com.bat
+++ com.bat
@@ -2,8 +2,8 @@
 git add .
 git commit -m " 666's spy " 2>nul
 git branch -M main
-git remote add origin https://github.com/Wwwaaallvvvyyy666/remotespy.git 2>nul
-git remote set-url origin https://github.com/Wwwaaallvvvyyy666/remotespy.git
+git remote add origin https://github.com/Wwwaaallvvvyyy666/rspy.git 2>nul
+git remote set-url origin https://github.com/Wwwaaallvvvyyy666/rspy.git
 git push -u origin main -f
 echo.
 echo Selesai! Push ke GitHub berhasil.
